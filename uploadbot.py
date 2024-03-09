d7158173d3b9ddee4be8d30a0480b21f3236718c
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2024-03-09 14:49:38 +0530

    Update and rename test.py to uploadbot.py

---
+++ uploadbot.py
@@ -0,0 +1,29 @@
+from pyrogram import Client , filters
+from pyrogram.types import Message
+import os
+
+bot = Client(
+    "bot",
+    api_id=6,
+    api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e",
+    bot_token=os.environ['BTOKEN']
+)
+
+CHANNEL_ID = -1001797893141
+BOT_USERNAME = "dynastyanimeuploaderbot" #WITOUT @
+
+@bot.on_message(filters.document & filters.user([2114972069, 5704299476]))
+def main(_,m : Message):
+    x = bot.send_document(CHANNEL_ID,m.document.file_id)
+    msg_id = x.id
+    m.reply(f"https://t.me/{BOT_USERNAME}?start={msg_id}")
+
+@bot.on_message(filters.command("start"))
+def start(_,m : Message):
+    msg_id = m.text.split(" ")[1]
+    msg_id = int(msg_id)
+    doc = bot.get_messages(CHANNEL_ID , msg_id)
+    doc.copy(m.chat.id)
+
+
+bot.run()
